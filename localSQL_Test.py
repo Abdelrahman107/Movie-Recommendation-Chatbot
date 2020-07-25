ac365bb6b1fd9336b0ef32771ec9976cd7e7ac03
Abdelrahman107 <abdelrahmanfangary@gmail.com>
2020-07-25 19:20:59 +0200

    added for testing database locally in mySQL_bench

---
+++ localSQL_Test.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+ 
+def saveToDB(name,age):
+  mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password=""
+  )
+  mycursor = mydb.cursor()
+  mycursor.execute("CREATE DATABASE movieuserdatabase")
+  #mycursor.execute("CREATE TABLE User (name VARCHAR(255), age int(20))")
+  sql = "INSERT INTO user (name, age) VALUES (%s, %s)"
+  val=(name,age)
+  mycursor.execute(sql, val)
+  mydb.commit()
+
+
+  
+
+
