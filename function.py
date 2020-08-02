8b11923378855310f4264ff7c8268fca3edba26f
unknown <mohamedhashem462@gmail.com>
2020-08-02 19:53:38 +0200

    updated function.py and rivescript

--- function.py
+++ function.py
@@ -1,22 +1,18 @@
-
 import requests
-global response
 
 
 
 def getMovie(genre):
-  
-    api = "https://run.mocky.io/v3/a6f18c23-7cee-4a7f-b4ac-11b9467af046" 
+    api = "https://run.mocky.io/v3/2e0ef401-7da0-42e7-acee-6fb958e9b9e0" 
     moviesData = requests.get(api).json()
-    #print(type(moviesData[0]['genres']))
-    #list=['abc']
-    #print(list)   
-    genre=genre[0].upper()
+    genre = genre.capitalize()
+    print(genre) 
+    stringOfMovies=""
     for movie in moviesData:
-        if genre in movie['genres']:
-            print(movie['title'])  
-            #list.insert(movie['title'])
+        if (genre in movie['genres']):
+            stringOfMovies = stringOfMovies  + "------" + movie['title']  
 
-    #response = requests.get(api).json()
-    #return response
+    return stringOfMovies
+
+
 
